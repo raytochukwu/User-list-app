f0d29144ef9c9183fd1610497125e40aa3939fdf
raytochukwu <ezeufotc@gmail.com>
2022-04-13 04:56:33 +0100

    update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,15 @@ import style from 'styled-components'
 const Layout = ({ children }) => {
   return (
     <Layouts>
+      <div className="header">
+        <img
+          style={{ width: '150px', marginTop: '10px' }}
+          src="https://uploads-ssl.webflow.com/610824d4fcb6d649baba751a/616ef5037713a882f83023ee_Proexe_logo_dark.svg"
+          alt=""
+        />
+      </div>
       <h1 className="dasboard">Dashboard</h1>
+
       {children}
     </Layouts>
   )
@@ -15,6 +23,10 @@ export default Layout
 const Layouts = style.div`
 padding-right: 20px;
 padding-left: 20px;
-
+.header{
+  display: flex;
+    align-items: center;
+    justify-content:center;
+}
 
 `
